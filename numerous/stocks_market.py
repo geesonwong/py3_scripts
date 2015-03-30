f39e45a09ab30655a0767bcb09085ea519882786
airsen <jason61719@gmail.com>
2015-03-30 16:52:10 +0800

    修改结束时间

--- numerous/stocks_market.py
+++ numerous/stocks_market.py
@@ -18,9 +18,9 @@ def main():
     metric_id = sys.argv[1]
     code = sys.argv[2]
     am_start = datetime.time(hour=9, minute=30)
-    am_end = datetime.time(hour=11, minute=30)
+    am_end = datetime.time(hour=11, minute=31)
     pm_start = datetime.time(hour=13, minute=00)
-    pm_end = datetime.time(hour=15, minute=00)
+    pm_end = datetime.time(hour=15, minute=1)
 
     while True:
         now_time = datetime.datetime.now().time()
