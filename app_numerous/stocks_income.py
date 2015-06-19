1c490a65a20e6bc77210f85aefb4caf254e26fc4
airsen <jason61719@gmail.com>
2015-06-19 16:59:08 +0800

    修改成本价

--- app_numerous/stocks_income.py
+++ app_numerous/stocks_income.py
@@ -26,7 +26,7 @@ def main():
     am_start = datetime.time(hour=9, minute=30)
     am_end = datetime.time(hour=11, minute=31)
     pm_start = datetime.time(hour=13, minute=0)
-    pm_end = datetime.time(hour=19, minute=1)
+    pm_end = datetime.time(hour=15, minute=1)
 
     while True:
         now_time = datetime.datetime.now().time()
